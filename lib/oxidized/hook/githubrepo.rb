ea98215571a137af973d68ffd5b48ecd54ac821b
Elvin Efendi <elvin.efendiev@shopify.com>
2015-12-04 03:01:09 -0500

    always fetch and merge origin/master before trying to push

--- lib/oxidized/hook/githubrepo.rb
+++ lib/oxidized/hook/githubrepo.rb
@@ -9,6 +9,17 @@ class GithubRepo < Oxidized::Hook
     log "Pushing local repository(#{repo.path})..."
     remote = repo.remotes['origin'] || repo.remotes.create('origin', cfg.remote_repo)
     log "to remote: #{remote.url}"
+
+    repo.fetch('origin', [repo.head.name], credentials: credentials)
+    merge_index = repo.merge_commits(repo.head.target, repo.branches['origin/master'].target)
+    Rugged::Commit.create(repo, {
+      parents: [repo.head.target, repo.branches['origin/master'].target],
+      tree: merge_index.write_tree(repo),
+      message: "Merge remote-tracking branch 'origin/master'",
+      update_ref: repo.head.name
+    })
+    repo.checkout_head(strategy: :force)
+
     remote.push(['refs/heads/master'], credentials: credentials)
   end
 end
